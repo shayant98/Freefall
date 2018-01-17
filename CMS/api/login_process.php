475ef2278459c9d2ede6bfad53bcdadcf57f7328
Shayant <shayant98@gmail.com>
2018-01-17 08:01:25 -0300

    CMS files
    Login is ook afgerond

---
+++ CMS/api/login_process.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+
+
+  if (isset($_POST['submit'])) {
+    include '../../api/dbc.php';
+
+     $username = mysqli_real_escape_string($conn, $_POST['username']);
+     $password = mysqli_real_escape_string($conn, $_POST['password']);
+
+
+    //Error Handler
+    if (empty($username) || empty($password)) {
+      header("Location: ../index.php?login=empty");
+      exit();
+    }else {
+      $sql = "SELECT * FROM fr_admin WHERE username = '$username'";
+      $result = mysqli_query($conn, $sql);
+      $resultCheck = mysqli_num_rows($result);
+
+      if ($resultCheck < 1) {
+        header("Location: ../index.php?login=error");
+        exit();
+      }else {
+        if ($row = mysqli_fetch_assoc($result)) {
+          //De-Hashing moet hier gedaan worden
+          if ($password != $row['password']) {
+            header("Location: ../index.php?login=error");
+            exit();
+          }elseif ($password == $row['password']) {
+            //login the user here
+            $_SESSION['a_name'] = $row['id'];
+            $_SESSION['a_surname'] = $row['name'];
+            $_SESSION['a_username'] = $row['surname'];
+
+
+              header("Location: ../dashboard.php");
+              exit();
+          }
+        }
+      }
+    }
+  }else {
+    header("Location: ../index.php?login=error");
+    exit();
+  }
+ ?>
---
+++ CMS/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>INSPINIA | Login</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
+
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+</head>
+
+<body class="gray-bg">
+
+    <div class="middle-box text-center loginscreen animated fadeInDown">
+        <div>
+            <div>
+
+                <h1 class="logo-name">IN+</h1>
+
+            </div>
+            <h3>Welcome to IN+</h3>
+            <p>Perfectly designed and precisely prepared admin theme with over 50 pages with extra new web app views.
+                <!--Continually expanded and constantly improved Inspinia Admin Them (IN+)-->
+            </p>
+            <p>Login in. To see it in action.</p>
+            <form class="m-t" role="form" action="api/login_process.php" method="post">
+                <div class="form-group">
+                    <input type="text" name="username" class="form-control" placeholder="username">
+                </div>
+                <div class="form-group">
+                    <input type="password" name="password" class="form-control" placeholder="password">
+                </div>
+                <button type="submit" class="btn btn-primary block full-width m-b" name="submit">Login</button>
+            </form>
+            <p class="m-t"> <small>Inspinia we app framework base on Bootstrap 3 &copy; 2014</small> </p>
+        </div>
+    </div>
+
+    <!-- Mainly scripts -->
+    <script src="js/jquery-2.1.1.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
