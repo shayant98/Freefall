73d4da5f8a405334dae54b80c3f57a22dec8b8c0
Shayant <shayant98@gmail.com>
2018-01-31 10:00:51 -0300

    boom boom

--- CMS/api/delete_project.php
+++ CMS/api/delete_project.php
@@ -3,6 +3,10 @@ include 'dbc.php';
 
 if (!empty($_GET['id'])) {
   $id = $_GET['id'];
+
+  $sql = mysqli_query($conn, "DELETE FROM fr_projects
+WHERE id=$id");
+header("Location: ../projecten.php");
 }else {
   echo "Geen Project gekozen";
 }
